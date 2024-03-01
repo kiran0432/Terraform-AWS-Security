bcd958d6cbb419e10f492c10e594deba9f39d0cd
Kiran Babu Kannedhara <55863987+kiran0432@users.noreply.github.com>
2024-03-01 19:31:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "example_server" {
+resource "aws_instance" "kiran-test" {
   ami           = "ami-0005e0cfe09cc9050"
   instance_type = "t2.micro"
 
